f6d5f0f1fda03a6d5754463dbcd11187a2d77afc
shaynemarusic <92888014+shaynemarusic@users.noreply.github.com>
2023-02-06 17:34:05 -0500

    Added login and registration features

--- main.go
+++ main.go
@@ -1,22 +1,103 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"log"
-    "net/http"
+	"net/http"
+	"time"
+
+	"github.com/golang-jwt/jwt"
 	"github.com/gorilla/mux"
-	"path"
-    "path/filepath"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
 
-func main() {
-    router := mux.NewRouter()
-    router.HandleFunc("/create", create).Methods("POST")
-    router.HandleFunc("/read", read).Methods("GET")
-    router.HandleFunc("/update", update).Methods("PUT")
-    router.HandleFunc("/delete", delete_).Methods("DELETE")
+var client *mongo.Client
 
-    err := http.ListenAndServe(":8080", router)
-    if err != nil {
-        log.Fatalln("There's an error with the server," err)
-    }
+var SECRET_KEY = []byte("gosecretkey")
+
+type User struct {
+	Name     string `json:"name" bson:"name"`
+	Email    string `json:"email" bson:"email"`
+	Password string `json:"password" bson:"password"`
+}
+
+// Takes in password, returns a hash
+func getHash(pwd []byte) string {
+	hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.MinCost)
+	if err != nil {
+		log.Println(err)
+	}
+	return string(hash)
+}
+
+// This does something???
+func GenerateJWT() (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	tokenString, err := token.SignedString(SECRET_KEY)
+	if err != nil {
+		log.Println("Error in JWT token generation")
+		return "", err
+	}
+	return tokenString, nil
+}
+
+// This function registers a new user
+func userRegister(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "application/json")
+	var user User
+	json.NewDecoder(request.Body).Decode(&user)
+	user.Password = getHash([]byte(user.Password))
+	collection := client.Database("udbo").Collection("user")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	result, _ := collection.InsertOne(ctx, user)
+	json.NewEncoder(response).Encode(result)
+}
+
+// This function logs a user in
+func userLogin(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "application/json")
+	var user User
+	var dbUser User
+	json.NewDecoder(request.Body).Decode(&user)
+	collection := client.Database("GODB").Collection("user")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err := collection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&dbUser)
+
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message":"` + err.Error() + `"}`))
+		return
+	}
+	userPass := []byte(user.Password)
+	dbPass := []byte(dbUser.Password)
+
+	passErr := bcrypt.CompareHashAndPassword(dbPass, userPass)
+
+	if passErr != nil {
+		log.Println(passErr)
+		response.Write([]byte(`{"response":"Wrong Password!"}`))
+		return
+	}
+	jwtToken, err := GenerateJWT()
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message":"` + err.Error() + `"}`))
+		return
+	}
+	response.Write([]byte(`{"token":"` + jwtToken + `"}`))
+
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/login/register", userRegister).Methods("POST")
+	router.HandleFunc("/login", userLogin).Methods("POST")
+
+	// err := http.ListenAndServe("localhost:4200", router)
+	// if err != nil {
+	//     log.Fatalln("There's an error with the server," err)
+	// }
 }
