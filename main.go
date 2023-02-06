6c6aaf6e5716021e68126b318eebe94e66de31b2
shaynemarusic <92888014+shaynemarusic@users.noreply.github.com>
2023-02-06 16:36:18 -0500

    Idk

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"log"
+    "net/http"
+	"github.com/gorilla/mux"
+	"path"
+    "path/filepath"
+)
 
+func main() {
+    router := mux.NewRouter()
+    router.HandleFunc("/create", create).Methods("POST")
+    router.HandleFunc("/read", read).Methods("GET")
+    router.HandleFunc("/update", update).Methods("PUT")
+    router.HandleFunc("/delete", delete_).Methods("DELETE")
+
+    err := http.ListenAndServe(":8080", router)
+    if err != nil {
+        log.Fatalln("There's an error with the server," err)
+    }
 }
