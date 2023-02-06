39edf635ee72808af5c60fb9f34fc56339ebb6c6
Christopher Tressler <92888194+ChrisTressler@users.noreply.github.com>
2023-02-06 15:55:24 -0500

    added officers page

--- SPCBSite/src/app/app.module.ts
+++ SPCBSite/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { OfficersComponent } from './officers/officers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    NavbarComponent
+    NavbarComponent,
+    OfficersComponent
   ],
   imports: [
     BrowserModule,
