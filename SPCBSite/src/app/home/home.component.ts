c94a67cd76b504e3ad25f1a9e51035d4edd2473f
Matthew Jung <90943304+YuumaHiraga@users.noreply.github.com>
2023-02-13 12:14:50 -0500

    Put club related images in carousel

--- SPCBSite/src/app/home/home.component.ts
+++ SPCBSite/src/app/home/home.component.ts
@@ -10,23 +10,23 @@ export class HomeComponent {
   images = [
     {
       imageSrc:
-        'https://images.unsplash.com/photo-1460627390041-532a28402358?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-      imageAlt: 'nature1',
+      '../../assets/IMG_0016.JPG',
+      imageAlt: 'GBMpic#16',
     },
     {
       imageSrc:
-        'https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-      imageAlt: 'nature2',
+      '../../assets/IMG_0015.JPG',
+      imageAlt: 'GBMpic#15',
     },
     {
       imageSrc:
-        'https://images.unsplash.com/photo-1640844444545-66e19eb6f549?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80',
-      imageAlt: 'person1',
+      '../../assets/IMG_0012.JPG',
+      imageAlt: 'GBMpic#12',
     },
     {
       imageSrc:
-        'https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-      imageAlt: 'person2',
+      '../../assets/IMG_0017.JPG',
+      imageAlt: 'GBMpic#17',
     },
   ]
 }
