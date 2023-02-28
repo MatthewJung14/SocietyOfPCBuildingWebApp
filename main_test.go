739273a0973db912e8b0b56c639361fa94914daa
Kushal0217 <kushalgaddam@ufl.edu>
2023-02-28 16:13:43 -0500

    Thank god and im not even relgious

--- main_test.go
+++ main_test.go
@@ -11,40 +11,43 @@ import (
 	"gorm.io/gorm"
 )
 
-func TestLoginHandler(t *testing.T) {
-	// create a test HTTP server
-	env := &Env{} // create a mock environment
-	router := http.NewServeMux()
-	router.HandleFunc("/user-login", env.userLogin)
-	server := httptest.NewServer(router)
-	defer server.Close()
-	user := User{Email: "testpass2@mail.com", Password: "testpass2"}
+func TestUserLogin(t *testing.T) {
+	// Define a mock environment for the function
+	env := Env{db: &gorm.DB{}}
+
+	// Define a mock user object with valid credentials
+	user := User{Email: "test@mail.com", Password: "testpass"}
 	body, _ := json.Marshal(user)
-	req, err := http.NewRequest("POST", server.URL+"/user-login", bytes.NewReader(body))
+
+	// Create a new HTTP POST request to the login endpoint with the mock user object as the body
+	req, err := http.NewRequest("POST", "/login", bytes.NewBuffer(body))
 	if err != nil {
 		t.Fatal(err)
 	}
 	req.Header.Set("Content-Type", "application/json")
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer res.Body.Close()
 
-	// Check the response status code
-	if res.StatusCode != http.StatusOK {
-		t.Errorf("Expected status 200 but got %d", res.StatusCode)
+	// Create a new HTTP response recorder to capture the response
+	rr := httptest.NewRecorder()
+
+	// Call the userLogin function with the mock environment, request, and response recorder
+	env.userLogin(rr, req)
+
+	// Check the status code of the response
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
 	}
 
-	// Check the response body
-	var response struct {
-		Token string `json:"token"`
+	// Check the content type of the response
+	expectedContentType := "application/json"
+	if contentType := rr.Header().Get("Content-Type"); contentType != expectedContentType {
+		t.Errorf("handler returned wrong content type: got %v want %v", contentType, expectedContentType)
 	}
-	if err := json.NewDecoder(res.Body).Decode(&response); err != nil {
-		t.Errorf("Failed to decode response body: %v", err)
-	}
-	if response.Token == "" {
-		t.Errorf("Expected non-empty token but got empty string")
+
+	// Check the response body for a JWT token and success message
+
+	expectedResponse := `{"response":"Successful"}`
+	if rr.Body.String() != expectedResponse {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expectedResponse)
 	}
 }
 
@@ -53,7 +56,7 @@ func TestUserRegister(t *testing.T) {
 	env := &Env{db: &gorm.DB{}}
 
 	// Create a new HTTP request with a POST method and a JSON payload
-	payload := strings.NewReader(`{"Email": "test2@mail.com", "Password": "test2pass"}`)
+	payload := strings.NewReader(`{"Email": "test@mail.com", "Password": "testpass"}`)
 	req := httptest.NewRequest("POST", "/register", payload)
 	req.Header.Set("Content-Type", "application/json")
 
@@ -69,8 +72,7 @@ func TestUserRegister(t *testing.T) {
 	}
 
 	// Check the response body
-	expectedBody := "Sucessful"
-	if recorder.Body.String() != expectedBody {
-		t.Errorf("Expected response body '%s' but got '%s'", expectedBody, recorder.Body.String())
+	if !strings.Contains(recorder.Body.String(), "Successful") {
+		t.Errorf("Expected response body to contain 'Successful' but got '%s'", recorder.Body.String())
 	}
 }
