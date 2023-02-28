c6ad80396bc4bd3408beea383da2143c8bfa4f30
Matthew Jung <90943304+YuumaHiraga@users.noreply.github.com>
2023-02-28 14:14:30 -0500

    Wrote Cypress test cases

--- SPCBSite/src/app/navbar/navbar.component.cy.ts
+++ SPCBSite/src/app/navbar/navbar.component.cy.ts
@@ -1,12 +1,22 @@
 import { NavbarComponent } from './navbar.component'
+import { createOutputSpy } from 'cypress/angular'
 
 describe('NavbarComponent', () => {
   it('mounts', () => {
     cy.mount(NavbarComponent)
   })
-  
-  it('stepper should have the home button', () => {
+
+  it('stepper should have the title', () => {
     cy.mount(NavbarComponent)
     cy.get('[data-cy=counter]').should('have.text', 'The Society of PC Building')
+  })
+  it('clicking the home button', () => {
+    cy.mount(NavbarComponent, {
+      componentProperties: {
+        change: createOutputSpy('changeSpy'),
+      },
+    })
+    cy.get('[data-cy=home]').click()
+    cy.get('@changeSpy').should('have.been.calledWith', 1)
   })
 })
