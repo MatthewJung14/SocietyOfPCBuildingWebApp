99fe17c5c8845c9c486d97212477fca8f1b50c70
Christopher Tressler <92888194+ChrisTressler@users.noreply.github.com>
2023-02-28 17:44:59 -0500

    Made it so that unit tests

--- SPCBSite/src/app/login/login.component.ts
+++ SPCBSite/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
