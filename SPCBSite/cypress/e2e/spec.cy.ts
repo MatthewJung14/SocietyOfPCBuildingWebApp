9ac68aac21faeff8d0b20b4163c6d2f37cd721ae
Matthew Jung <90943304+YuumaHiraga@users.noreply.github.com>
2023-03-01 11:13:31 -0500

    E2E successfully loads

--- SPCBSite/cypress/e2e/spec.cy.ts
+++ SPCBSite/cypress/e2e/spec.cy.ts
@@ -2,4 +2,14 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:4200/')
   })
+
+  // it('Does not do much!', () => {
+  //   expect(true).to.equal(true)
+  // })
+
+
+  it('successfully loads', () => {
+    cy.visit('http://localhost:4200/login')
+  })
+  
 })
