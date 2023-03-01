41a044f8d214e481394b040d7ea63a2706615e09
Christopher Tressler <92888194+ChrisTressler@users.noreply.github.com>
2023-03-01 00:53:41 -0500

    fixed routing test case

--- SPCBSite/src/app/app-routing.spec.ts
+++ SPCBSite/src/app/app-routing.spec.ts
@@ -9,6 +9,10 @@ import { LoginComponent } from './login/login.component';
 import { OfficersComponent } from './officers/officers.component';
 import { RentComponent } from './rent/rent.component';
 import { SignupComponent } from './signup/signup.component';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { CaraouselComponent } from '../app/caraousel/caraousel.component'
 
 describe("App Routing", ()=>{
     let router: Router;
@@ -17,14 +21,16 @@ describe("App Routing", ()=>{
 
     beforeEach(waitForAsync(()=>{
       TestBed.configureTestingModule({
-        imports: [RouterTestingModule.withRoutes(routes)],
+        imports: [RouterTestingModule.withRoutes(routes), MatToolbarModule, MatCardModule],
         declarations: [
             AppComponent, 
             HomeComponent,
             LoginComponent,
             OfficersComponent,
             RentComponent,
-            SignupComponent
+            SignupComponent,
+            NavbarComponent,
+            CaraouselComponent
         ]
       }).compileComponents();
     }));
