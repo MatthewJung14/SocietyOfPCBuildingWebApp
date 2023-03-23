a4aa5b9c19634e2a6e622c97c01e4aea5c9e5e6b
Christopher Tressler <92888194+ChrisTressler@users.noreply.github.com>
2023-03-22 23:30:59 -0400

    added calendar to routing

--- SPCBSite/src/app/app-routing.module.ts
+++ SPCBSite/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalendarComponent } from './calendar/calendar.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { OfficersComponent } from './officers/officers.component';
@@ -13,6 +14,7 @@ export const routes: Routes = [
   {path: 'rent', component: RentComponent},
   {path: 'signup', component: SignupComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'calendar', component: CalendarComponent}
 ];
 
 @NgModule({
