9ff7b8048390fe71ccaab9d7dd21b7aa7ddee25a
Matthew Jung <90943304+YuumaHiraga@users.noreply.github.com>
2023-03-26 11:17:22 -0400

    Created Settings button test case

--- SPCBSite/src/app/settings/settings.component.ts
+++ SPCBSite/src/app/settings/settings.component.ts
@@ -7,7 +7,8 @@ import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css']
+  styleUrls: ['./settings.component.css'],
+  providers: [AuthService],
 })
 
 @Injectable({
