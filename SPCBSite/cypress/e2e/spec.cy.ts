d8d11232e875cc9268b1b9ae2c499a4f8041afa9
Christopher Tressler <92888194+ChrisTressler@users.noreply.github.com>
2023-04-19 17:12:15 -0400

    added more renting test cases

--- SPCBSite/cypress/e2e/spec.cy.ts
+++ SPCBSite/cypress/e2e/spec.cy.ts
@@ -89,33 +89,35 @@ describe('template spec', () => {
     cy.get('.string-array__item').contains(`Reserved by: ${firstName} ${lastName} (${email})`);
   });
 
-  it('should prevent reserving a time slot with the same email', () => {
-    const firstName1 = 'John';
-    const lastName1 = 'Doe';
-    const email1 = 'johndoe@example.com';
-    const timeSlot1 = '9AM';
-    const firstName2 = 'Jane';
-    const lastName2 = 'Doe';
-    const email2 = 'janedoe@example.com';
-    const timeSlot2 = '10AM';
-
-    cy.visit('http://localhost:3200');
-    cy.contains('RENT A PC').click()
-    cy.get('input[name="firstName"]').type(firstName1);
-    cy.get('input[name="lastName"]').type(lastName1);
-    cy.get('input[name="email"]').type(email1);
-    cy.get('input[name="timeSlot"]').type(timeSlot1);
-    cy.get('button').contains('SUBMIT TIME').click();
-
-    cy.get('input[name="firstName"]').type(firstName2);
-    cy.get('input[name="lastName"]').type(lastName2);
-    cy.get('input[name="email"]').type(email1);
-    cy.get('input[name="timeSlot"]').type(timeSlot2);
-    cy.get('button').contains('SUBMIT TIME').click();
-
-    cy.get('.string-array__item').contains(timeSlot1).should('not.exist');
-    cy.get('.string-array__item').contains(`Reserved by: ${firstName1} ${lastName1} (${email1})`);
-    cy.get('.string-array__item').contains(timeSlot2).should('exist').and('not.contain', email1);
+  describe('RentComponent', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:3200');
+      cy.contains('RENT A PC').click()
+    });
+  
+    it('should have form fields', () => {
+      cy.get('input[name="firstName"]').should('exist');
+      cy.get('input[name="lastName"]').should('exist');
+      cy.get('input[name="email"]').should('exist');
+      cy.get('input[name="timeSlot"]').type('11PM', { force: true });
+    });
+  
+    it('should submit form with valid data', () => {
+      cy.get('input[name="firstName"]').type('John');
+      cy.get('input[name="lastName"]').type('Doe');
+      cy.get('input[name="email"]').type('johndoe@example.com');
+      cy.get('input[name="timeSlot"]').type('8AM', { force: true });
+      cy.get('button').contains('SUBMIT TIME').click();
+    });
+  
+    it('should not submit form with invalid data', () => {
+      cy.get('input[name="firstName"]').type('John');
+      cy.get('input[name="lastName"]').type('Doe');
+      cy.get('input[name="email"]').type('johndoe@example.com');
+      cy.get('button').contains('SUBMIT TIME').click();
+      cy.get('.string-array__item').contains('johndoe@example.com').should('not.exist');
+      cy.url().should('not.include', '/home');
+    });
   });
   
 })
