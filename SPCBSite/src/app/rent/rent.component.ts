0fefe3dd051da3ae72bbb5221f4065112b37fc27
Christopher Tressler <92888194+ChrisTressler@users.noreply.github.com>
2023-04-19 01:16:40 -0400

    added full front end functionality for renting times

--- SPCBSite/src/app/rent/rent.component.ts
+++ SPCBSite/src/app/rent/rent.component.ts
@@ -11,8 +11,8 @@ export class RentComponent {
   email: string;
   timeSlot: string;
 
-  reservedSlots: { email: string, timeSlot: string }[] = [];
   strings: string[] = ['TIMES AVAILABLE', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', '9PM', '10PM']; // Define the array of strings
+  emails: string[] = [];
 
   constructor() {
     this.firstName = "";
@@ -27,22 +27,18 @@ export class RentComponent {
   
     if (index !== -1) {
       // If the selected time slot is in the array, check if the email is already in the reserved slots
-      const reservedSlots = this.strings.filter(str => str.includes(this.email));
+      const reservedSlots = this.emails.filter(str => str.includes(this.email));
       if (reservedSlots.length > 0) {
         console.log(`Email ${this.email} has already reserved a time slot.`);
         return;
       }
   
-      // If the email is not in the reserved slots, remove the time slot from the array and add the email and time slot as a single string
+      // If the email is not in the reserved slots, add the email to the emails array and remove the time slot from the strings array
+      this.emails.push(this.email);
       this.strings.splice(index, 1);
-      const reservation = `${this.email}: ${this.timeSlot}`;
-      this.strings.push(reservation);
   
       // Store the updated strings array in local storage
       localStorage.setItem('strings', JSON.stringify(this.strings));
     }
   }
-  
-  
-  
 }
