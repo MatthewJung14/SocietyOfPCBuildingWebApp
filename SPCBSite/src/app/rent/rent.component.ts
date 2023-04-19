92d012887c49c2cd32963887cc07a374cf4c7769
Christopher Tressler <92888194+ChrisTressler@users.noreply.github.com>
2023-04-19 01:00:08 -0400

    changed time slots to be proper

--- SPCBSite/src/app/rent/rent.component.ts
+++ SPCBSite/src/app/rent/rent.component.ts
@@ -11,7 +11,7 @@ export class RentComponent {
   email: string;
   timeSlot: string;
 
-  strings: string[] = ['TIMES AVAILABLE', '10PM', '11PM']; // Define the array of strings
+  strings: string[] = ['TIMES AVAILABLE', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', '9PM', '10PM']; // Define the array of strings
 
   constructor() {
     this.firstName = "";
