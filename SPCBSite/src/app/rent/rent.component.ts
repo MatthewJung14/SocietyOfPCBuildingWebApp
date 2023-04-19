edc98fe366a7094a003931e2ea8e2f5f201526c5
Christopher Tressler <92888194+ChrisTressler@users.noreply.github.com>
2023-04-19 01:04:39 -0400

    added reserved slots

--- SPCBSite/src/app/rent/rent.component.ts
+++ SPCBSite/src/app/rent/rent.component.ts
@@ -11,6 +11,7 @@ export class RentComponent {
   email: string;
   timeSlot: string;
 
+  reservedSlots: { email: string, timeSlot: string }[] = [];
   strings: string[] = ['TIMES AVAILABLE', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', '9PM', '10PM']; // Define the array of strings
 
   constructor() {
@@ -21,15 +22,27 @@ export class RentComponent {
   }
 
   submitTime() {
-    // Check if the entered value is present in the array
+    // Check if the selected time slot is in the strings array
     const index = this.strings.indexOf(this.timeSlot);
+  
     if (index !== -1) {
-      // Remove the element from the array
+      // If the selected time slot is in the array, check if the email is already in the reserved slots
+      const reservedSlots = this.strings.filter(str => str.includes(this.email));
+      if (reservedSlots.length > 0) {
+        console.log(`Email ${this.email} has already reserved a time slot.`);
+        return;
+      }
+  
+      // If the email is not in the reserved slots, remove the time slot from the array and add the email and time slot as a single string
       this.strings.splice(index, 1);
-      console.log(`Removed ${this.timeSlot} from the array.`);
-    } else {
-      console.log(`${this.timeSlot} not found in the array.`);
+      const reservation = `${this.email}: ${this.timeSlot}`;
+      this.strings.push(reservation);
+  
+      // Store the updated strings array in local storage
+      localStorage.setItem('strings', JSON.stringify(this.strings));
     }
   }
+  
+  
   
 }
