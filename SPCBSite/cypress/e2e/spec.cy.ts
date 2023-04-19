8847fe7007ff90d428089503cd4b23d5fecc867b
Matthew Jung <90943304+YuumaHiraga@users.noreply.github.com>
2023-04-19 16:37:10 -0400

    Added rent a pc cases to the E2E file

--- SPCBSite/cypress/e2e/spec.cy.ts
+++ SPCBSite/cypress/e2e/spec.cy.ts
@@ -83,6 +83,52 @@ describe('template spec', () => {
     cy.url().should('eq', 'http://localhost:3200/home')
   })
 
+  it('should reserve a time slot', () => {
+    const firstName = 'John';
+    const lastName = 'Doe';
+    const email = 'johndoe@example.com';
+    const timeSlot = '9AM';
 
+    cy.visit('http://localhost:3200');
+    cy.contains('RENT A PC').click()
+    cy.get('input[name="firstName"]').type(firstName);
+    cy.get('input[name="lastName"]').type(lastName);
+    cy.get('input[name="email"]').type(email);
+    cy.get('input[name="timeSlot"]').type(timeSlot);
+
+    cy.get('button').contains('SUBMIT TIME').click();
+
+    cy.get('.string-array__item').contains(timeSlot).should('not.exist');
+    cy.get('.string-array__item').contains(`Reserved by: ${firstName} ${lastName} (${email})`);
+  });
+
+  it('should prevent reserving a time slot with the same email', () => {
+    const firstName1 = 'John';
+    const lastName1 = 'Doe';
+    const email1 = 'johndoe@example.com';
+    const timeSlot1 = '9AM';
+    const firstName2 = 'Jane';
+    const lastName2 = 'Doe';
+    const email2 = 'janedoe@example.com';
+    const timeSlot2 = '10AM';
+
+    cy.visit('http://localhost:3200');
+    cy.contains('RENT A PC').click()
+    cy.get('input[name="firstName"]').type(firstName1);
+    cy.get('input[name="lastName"]').type(lastName1);
+    cy.get('input[name="email"]').type(email1);
+    cy.get('input[name="timeSlot"]').type(timeSlot1);
+    cy.get('button').contains('SUBMIT TIME').click();
+
+    cy.get('input[name="firstName"]').type(firstName2);
+    cy.get('input[name="lastName"]').type(lastName2);
+    cy.get('input[name="email"]').type(email1);
+    cy.get('input[name="timeSlot"]').type(timeSlot2);
+    cy.get('button').contains('SUBMIT TIME').click();
+
+    cy.get('.string-array__item').contains(timeSlot1).should('not.exist');
+    cy.get('.string-array__item').contains(`Reserved by: ${firstName1} ${lastName1} (${email1})`);
+    cy.get('.string-array__item').contains(timeSlot2).should('exist').and('not.contain', email1);
+  });
   
 })
